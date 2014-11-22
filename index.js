94981d9032087ab007339364cfa9f2f0eadc3cc8
Nitish Hooda <thenitish23121991@yahoo.in>
2014-11-22 21:47:10 +0530

    done

--- index.js
+++ index.js
@@ -4,4 +4,9 @@ setTimeout(function(){
 $('.overlay_result_container').addClass('show_overlay_result');
 },1300);
 
+$('.overlay_button').bind('click',function(){
+$('.graph_lightbox').addClass('show');
+$('.lightbox_box').addClass('show');
+});
+
 });
