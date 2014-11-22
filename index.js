6102bbd8e60747bd7c8d7d0c4b9cd0e9a247cc8b
Nitish Hooda <thenitish23121991@yahoo.in>
2014-11-23 04:44:04 +0530

    done

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+var boxes_arr = new Array();
+
 setTimeout(function(){
 $('.overlay_result_container').addClass('show_overlay_result');
 },1300);
@@ -12,5 +14,23 @@ $('.lightbox_box').addClass('show');
 $('.lightbox_box').bind('click',function(){
 $('.graph_lightbox, .lightbox_box').removeClass('show');
 });
+
+for(var i=1;i=<20;i++){
+boxes_arr.push(i);
+}
+
+
+function shuffle_array(arr){
+
+var rnum = Math.floor((Math.random()*arr.length)+1);
+console.log(rnum);
+
+}
+
+
+setTimeout(function(){
+shuffle_array(boxes_arr);
+},600);
+
 
 });
