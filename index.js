8d82a949c2655ccf2981e0e920e5710d2de145cf
Nitish Hooda <thenitish23121991@yahoo.in>
2014-11-23 08:30:42 +0530

    done

--- index.js
+++ index.js
@@ -22,15 +22,37 @@ for(var i=1;i<=25;i++){
 boxes_arr.push(i);
 }
 
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+
 function shuffle_array(arr){
 
 if(arr.length != 0){
-var rnum = Math.floor(Math.random()*arr.length);
+shuffle(boxes_arr);
+var rnum = boxes_arr[0];
 $('.graph_lightbox').addClass('show');
 $('#lightbox_box_'+rnum).addClass('show');
-boxes_arr.splice(arr[rnum]-2,1);
-console.log(boxes_arr);
+boxes_arr.splice(0,1);
 console.log(rnum);
+console.log(boxes_arr);
 }else{
 clearInterval(clear_interval);
 }
