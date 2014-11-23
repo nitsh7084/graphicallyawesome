48850a0a1d9bce8365a4ab54ee5d927f7fc25512
Nitish Hooda <thenitish23121991@yahoo.in>
2014-11-23 05:41:57 +0530

    done

--- index.js
+++ index.js
@@ -20,32 +20,6 @@ for(var i=1;i<=25;i++){
 boxes_arr.push(i);
 }
 
-function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex ;
-
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-
-  return array;
-}
-
-shuffle(boxes_arr);
-
-clear_interval = setInterval(function(){
-shuffle(boxes_arr);
-console.log(boxes_arr);
-},600);
-
 function shuffle_array(arr){
 
 if(arr.length != 0){
@@ -59,13 +33,10 @@ clearInterval(clear_interval);
 
 }
 
-/*
-
 clear_interval = setInterval(function(){
 shuffle_array(boxes_arr);
 },600);
 
-*/
 
 
 });
